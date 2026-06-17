6ab9c8b0b12b91cbe2c0890ac40969acfa8b271d
Litomyx <wild.jesse2@gmail.com>
2026-06-17 16:39:22 +0200

    removed github button

--- app/components/DAQheader.vue
+++ app/components/DAQheader.vue
@@ -37,17 +37,6 @@ const items = computed<NavigationMenuItem[]>(() => [
 
     <template #right>
       <UColorModeButton />
-
-      <UTooltip text="Open on GitHub" :kbds="['meta', 'G']">
-        <UButton
-          color="neutral"
-          variant="ghost"
-          to="https://github.com/nuxt/ui"
-          target="_blank"
-          icon="i-simple-icons-github"
-          aria-label="GitHub"
-        />
-      </UTooltip>
     </template>
   </UHeader>
 </template>
